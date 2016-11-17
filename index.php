2142a95ab6b738051142a6c20244f19f9bb265ed
yurenhen <yurenchen@yeah.net>
2016-11-18 00:41:25 +0800

    fix bug:  *.png folder display as png file
    folder named as 'xxx.png' should display folder ico

--- index.php
+++ index.php
@@ -135,8 +135,13 @@ function display_block( $file )
 {
 	global $ignore_file_list, $ignore_ext_list, $force_download;
 	
-	$file_ext = getFileExt($file);
-	if( !$file_ext AND is_dir($file)) $file_ext = "dir";
+	//$file_ext = getFileExt($file);
+	//if( !$file_ext AND is_dir($file)) $file_ext = "dir";
+	if( is_dir($file))
+		$file_ext = "dir";
+	else
+		$file_ext = getFileExt($file);
+
 	if(in_array($file, $ignore_file_list)) return;
 	if(in_array($file_ext, $ignore_ext_list)) return;
 	
